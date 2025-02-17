d50cc38197357688c4ab15f9d8c4bdf3fed910d0
Emanuela Calderone <emanuelacalderone3006@gmail.com>
2025-02-17 20:11:50 +0100

    create cards + add css (no bonus)

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Card = ({ language }) => {
+    return (
+        <div className="card">
+            <h2>{language.title}</h2>
+            <p>{language.description}</p>
+        </div>
+    );
+};
+
+export default Card;
